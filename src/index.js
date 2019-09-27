491ff60cebf3b996f297a65028d74e6f26d8360c
Chih-Yu <lcd010308@gmail.com>
2019-09-27 22:17:51 +0800

    Add bootstrap-like container

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
-import Router from './Router'
 
-import {createStore, applyMiddleware} from 'redux'
-import Reducers from './Redux'
-import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
-let store = createStore(Reducers, applyMiddleware(thunk))
-
-ReactDOM.render(<Provider store={store}><Router /></Provider>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
