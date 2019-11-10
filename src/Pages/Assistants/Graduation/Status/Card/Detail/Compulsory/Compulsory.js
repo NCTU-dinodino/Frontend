550f262c7731e26cd7b5b15444d0633af2b2bc1e
marmot0814 <s1357andy871207@gmail.com>
2019-11-11 00:08:27 +0800

    畢業預審功能拆成畢業預審和修課狀況

--- src/Pages/Assistants/Graduation/Status/Card/Detail/Compulsory/Compulsory.js
+++ src/Pages/Assistants/Graduation/Status/Card/Detail/Compulsory/Compulsory.js
@@ -10,17 +10,11 @@ const styles = theme => ({
 })
 
 class Compulsory extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-
-    }
-  }
 
   render() {
     const { student } = this.props
     return (
-      (parseInt(student.net_media) === 0 || parseInt(student.net_media) === 1) ? <WebNet student={student} /> : <OtherProgram student={student} />
+      (parseInt(student.net_media, 10) === 0 || parseInt(student.net_media, 10) === 1) ? <WebNet student={student} /> : <OtherProgram student={student} />
     )
   }
 }
