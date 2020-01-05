831012e31973a94280d91d2b659adf0bdd524670
marmot0814 <32301085+marmot0814@users.noreply.github.com>
2020-01-05 16:17:46 +0800

    Feat/assistant/proj (#45)
    * test score csv download
    
    * assistant proj score test
    
    * fix post paylad format bug
    
    * hightlight function
    
    * filter function
    
    * ignore when student comment is null
    
    * container width
    
    * 0分bug, 分數低於60和90以上需要評論

--- src/Redux/Assistants/Actions/Project/Score.js
+++ src/Redux/Assistants/Actions/Project/Score.js
@@ -1,4 +1,5 @@
 import { createAction } from 'redux-actions'
+import axios from 'axios';
 // import axios from 'axios'
 
 // export const store_teachers = createAction('STORE_TEACHERS')
@@ -18,7 +19,38 @@ import { createAction } from 'redux-actions'
 // }
 
 export const score_handle_change = createAction('SCORE_HANDLE_CHANGE');
+export const set_scores = createAction('SET_SCORES')
 
 export const scoreHandleChange = (payload) => dispatch => {
-    dispatch(score_handle_change(payload));
+  dispatch(score_handle_change(payload));
+}
+
+export const fetchScore = payload => dispatch => {
+  axios.post('/assistants/research/gradeList', payload).then( res => {
+    dispatch(score_handle_change({
+      scores: res.data
+    }))
+  }).catch( err => {
+    console.log(err)
+  })
+}
+
+export const fetchCsv = payload => dispatch => {
+  dispatch(score_handle_change({csvDone: false}))
+  axios.post('/assistants/research/gradeDownload', payload).then(res => {
+    let data = res.data, csvArr = []
+    csvArr.push(['專題名稱', '老師', '姓名', '學號', '成績', '評語'])
+    for (let i = 0; i < data.length; i++) {
+      csvArr.push([data[i].research_title, data[i].tname, data[i].sname, data[i].student_id, data[i].score, data[i].comment])
+    }
+    dispatch(score_handle_change({csvArr: csvArr, csvDone: true}))
+  })
+}
+
+export const setScores = payload => dispatch => {
+  axios.post('/assistants/research/setScore', payload).then( res => {
+    dispatch(set_scores(payload))
+  }).catch( err => {
+    console.log(err)
+  })
 }
