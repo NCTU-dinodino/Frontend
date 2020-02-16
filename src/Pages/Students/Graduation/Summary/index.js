7bd5fac192e2584ee655db8c30db983517b630c2
Chih-Yu <lcd010308@gmail.com>
2020-02-17 00:33:24 +0800

    Fix Summary

--- src/Pages/Students/Graduation/Summary/index.js
+++ src/Pages/Students/Graduation/Summary/index.js
@@ -55,10 +55,10 @@ const styles = theme => ({
   }
 })
 
-const CreditProgress = ({ overview }) => (
+const CreditProgress = ({ acquire, require }) => (
   <CircularProgressbar
-    percentage={100 * overview.total / overview.total_require}
-    text={`畢業 ${overview.total}/${overview.total_require}`}
+    percentage={100 * acquire / require}
+    text={`畢業 ${acquire}/${require}`}
     initialAnimation
     styles={{
       path: { stroke: '#34855e' },
@@ -74,7 +74,7 @@ const ColorInstruction = withStyles(styles)(({ classes, color, text }) => (
   </React.Fragment>
 ))
 
-const Index = ({ overview, reviewStatus, rejectReason, englishStatus, forAssistant, idCard, classes, mobile }) => {
+const Index = ({ courseDetail, reviewStatus, rejectReason, englishStatus, forAssistant, idCard, classes, mobile }) => {
   const reviewStatusText = {
     0: '未送審',
     1: '審核中',
@@ -94,7 +94,10 @@ const Index = ({ overview, reviewStatus, rejectReason, englishStatus, forAssista
     <React.Fragment>
       <Grid item xs={12} container justify='center' className={classes.creditProgressRow}>
         <Grid item xs={6} sm={4}>
-          <CreditProgress overview={overview} />
+          <CreditProgress
+            acquire={courseDetail.total && courseDetail.total.acquire}
+            require={courseDetail.total && courseDetail.total.require}
+          />
         </Grid>
       </Grid>
       <Grid item xs={12} container>
@@ -106,7 +109,7 @@ const Index = ({ overview, reviewStatus, rejectReason, englishStatus, forAssista
           </Grid>
           <Grid item xs={12} container alignItems='center'>
             <ColorInstruction color={classes.greyGreen} text='免修或抵免' />
-            <ColorInstruction color={classes.grey} text='待確認或重複' />
+            <ColorInstruction color={classes.grey} text='待確認' />
           </Grid>
         </Grid>
         <Grid item xs={4} container justify='flex-end' alignItems='center'>
@@ -141,7 +144,7 @@ const Index = ({ overview, reviewStatus, rejectReason, englishStatus, forAssista
         <ColorInstruction color={classes.red} text='未通過' />
         <ColorInstruction color={classes.purple} text='當期' />
         <ColorInstruction color={classes.greyGreen} text='免修或抵免' />
-        <ColorInstruction color={classes.grey} text='待確認或重複' />
+        <ColorInstruction color={classes.grey} text='待確認' />
       </Grid>
       <Grid item md={12} container className={classes.reviewRow}>
         <Grid item md={9}>
@@ -174,10 +177,13 @@ const Index = ({ overview, reviewStatus, rejectReason, englishStatus, forAssista
       </Grid>
       <Grid item md={12} spacing={24} container alignItems='center'>
         <Grid item md={3} lg={2}>
-          <CreditProgress overview={overview} />
+          <CreditProgress
+            acquire={courseDetail.total && courseDetail.total.acquire}
+            require={courseDetail.total && courseDetail.total.require}
+          />
         </Grid>
         <Grid item md={9} lg={10}>
-          <DetailProgress overview={overview} />
+          <DetailProgress courseDetail={courseDetail} />
         </Grid>
       </Grid>
     </React.Fragment>
@@ -185,7 +191,7 @@ const Index = ({ overview, reviewStatus, rejectReason, englishStatus, forAssista
 }
 
 const mapStateToProps = (state) => ({
-  overview: state.Student.Graduation.detail.overview,
+  courseDetail: state.Student.Graduation.detail.data,
   reviewStatus: state.Student.Graduation.getReview.status,
   rejectReason: state.Student.Graduation.getReview.rejectReason,
   englishStatus: state.Student.Graduation.english.status,
