7c06df62c4bd0d5ffd0caf29a0bf8dc41a19e9d6
gamerslouis <32395144+gamerslouis@users.noreply.github.com>
2020-02-24 22:50:09 +0800

    Remove student popular courses recomand (#48)

--- src/Redux/Students/Reducers/Recommend/index.js
+++ src/Redux/Students/Reducers/Recommend/index.js
@@ -2,12 +2,10 @@ import { handleActions } from 'redux-actions'
 
 const initialState = {
   recommendCourses: [],
-  popularCourses: []
 }
 
 export default handleActions({
   STORE_RECOMMEND_COURSES: (state, action) => ({ ...state, recommendCourses: action.payload }),
-  STORE_POPULAR_COURSES: (state, action) => ({ ...state, popularCourses: action.payload }),
   UPDATE_RATING: (state, action) => {
     let index = action.payload
     let updated = [...state.recommendCourses]
