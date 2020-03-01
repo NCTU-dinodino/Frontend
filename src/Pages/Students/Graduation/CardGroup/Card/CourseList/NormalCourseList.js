d851825366d2a3ab29bdb69aeebcc623fbb0a858
Chih-Yu <lcd010308@gmail.com>
2020-03-01 23:44:51 +0800

    修改課程學分顯示的判定

--- src/Pages/Students/Graduation/CardGroup/Card/CourseList/NormalCourseList.js
+++ src/Pages/Students/Graduation/CardGroup/Card/CourseList/NormalCourseList.js
@@ -3,12 +3,11 @@ import React from 'react'
 import { NormalCoursePopover } from '../CoursePopover'
 
 /* 
+ * 有些課程因為學分要求，會被拆成兩份
  * 物理一門4學分，資工只要求3學分
  * 微處理機3學分，資工只要求2學分
- * DLAB3學分，資工只要求2學分
  * 多餘的1學分會放到專業選修或其他選修
  */
-const specialCourse = ['物理(一)', '物理(二)', '物理(一)榮譽班', '物理(二)榮譽班', '微處理機系統實驗', '數位電路實驗']
 
 const Index = ({ courses, title }) => (
   <React.Fragment>
@@ -16,7 +15,7 @@ const Index = ({ courses, title }) => (
       courses.map((course, index) => (
         <NormalCoursePopover
           key={index}
-          label={(specialCourse.includes(course.cn) && course.realCredit > 0)
+          label={(course.realCredit < course.originalCredit && course.realCredit > 0)
             ? `${course.cn}  ${course.realCredit}學分`
             : course.cn
           }
