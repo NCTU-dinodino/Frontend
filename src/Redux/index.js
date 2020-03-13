c25146e31ff3f1a2b12eff1b4cd50885cddbc7cc
Chih-Yu <lcd010308@gmail.com>
2020-03-13 23:14:17 +0800

    修正自動登出

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux'
 
+import Index from './Index/Reducers'
 import Assistant from './Assistants/Reducers'
 import Student from './Students/Reducers'
 import Teacher from './Teachers/Reducers'
 import Bulletins from './Bulletins/Reducers'
 
 export default combineReducers({
+  Index,
   Assistant,
   Student,
   Teacher,
