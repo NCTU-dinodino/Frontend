d7647c5a16a48c3931236dbb0cd02c872011bf91
marmot0814 <32301085+marmot0814@users.noreply.github.com>
2020-07-31 21:11:22 +0800

    Assistant time setting (#65)

--- src/Redux/Assistants/Actions/Setting/Time.js
+++ src/Redux/Assistants/Actions/Setting/Time.js
@@ -3,6 +3,21 @@ import axios from 'axios'
 
 export const time_handle_change = createAction('TIME_HANDLE_CHANGE');
 
+export const fetchTime = () => dispatch => {
+  axios.get('/assistants/getTimes').then( res =>
+    dispatch(time_handle_change(res.data))
+  ).catch( err => {
+    window.alert("獲取時間失敗")
+    console.log(err)
+  })
+}
+
 export const timeHandleChange = (payload) => dispatch => {
-  dispatch(time_handle_change(payload));
+  axios.post('/assistants/setTimes', payload).then( res => {
+    dispatch(time_handle_change(payload))
+    window.alert("修改成功!")
+  }).catch( err => {
+    window.alert("修改失敗!")
+    console.log(err)
+  })
 }
